54aadcced55078788379ca93806e86df5eee7d3c
OlgaVashurina <ovashurina@gmail.com>
2024-06-07 15:47:01 +0200

    New07/06

--- src/lesson_17/HW17DogTest.java
+++ src/lesson_17/HW17DogTest.java
@@ -3,13 +3,13 @@ package lesson_17;
 public class HW17DogTest {
     public static void main(String[] args) {
 
-        HW17Dog HW17Dog1 = new HW17Dog("Улли", 70);
-        HW17Dog HW17Dog2 = new HW17Dog("Шарик", 80);
-        HW17Dog HW17Dog3 = new HW17Dog("Шуша", 90);
-        HW17Dog HW17Dog4 = new HW17Dog("Оливер", 100);
-        HW17Dog HW17Dog5 = new HW17Dog("Доби", 110);
+        HW17Dog HW17Dog1 = new HW17Dog("Улли", 30);
+        HW17Dog HW17Dog2 = new HW17Dog("Шарик", 40);
+        HW17Dog HW17Dog3 = new HW17Dog("Шуша", 50);
+        HW17Dog HW17Dog4 = new HW17Dog("Оливер", 600);
+        HW17Dog HW17Dog5 = new HW17Dog("Доби", 80);
 
-        int[] barriers = {60, 70, 80, 90, 100, 110, 120};
+        int[] barriers = {70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170};
 
         for (int barrier : barriers) {
             HW17Dog1.getBarrier(barrier);
